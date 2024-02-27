009fd3ae296ee453b90da37c3a95bf867703db8e
mate2024 <mate.natroshvili.2016@wp.pl>
2024-02-28 00:43:33 +0100

    komitis saxeli

---
+++ js/mine.js
@@ -0,0 +1,11 @@
+ swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
+  
